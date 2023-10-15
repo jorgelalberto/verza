af0c1bf58c436884d4c8eb557756a9baaf5f3140
Jorge Luis Alberto <92881097+jorgelalberto@users.noreply.github.com>
2023-10-15 02:56:03 -0400

    redo organization

--- src/api/routes/employee.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const Employee = require('../models/employee')
-
-module.exports = router
--- src/api/routes/employer.js
+++
@@ -1,5 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const Employer = require('../models/employer')
-
-module.exports = router
