5886d8a3ef191784db4414e8e811727e363d3343
Jorge L. Alberto <jorgealberto@ufl.edu>
2023-10-15 03:07:46 -0400

    testing index.js

---
+++ server/src/index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const { dirname } = require('path')
+
+app.listen(3000)
+
+app.get('/', (req, res) => {
+    res.sendFile('./api/views/index.html', {root: __dirname})
+})
+
+// error checking
+app.use((req, res) => {
+    res.sendStatus(404)
+})
